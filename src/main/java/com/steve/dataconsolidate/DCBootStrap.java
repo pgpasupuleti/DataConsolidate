746fa7274e80dc09d6fd894683f599bf2b4a00dd
Praveen Kumar pasupuleti <pgpasupuleti@gmail.com>
2015-03-25 22:59:13 -0700

    Read Input data Directory Path from property file.

--- src/main/java/com/steve/dataconsolidate/DCBootStrap.java
+++ src/main/java/com/steve/dataconsolidate/DCBootStrap.java
@@ -1,5 +1,6 @@
 package com.steve.dataconsolidate;
 
+import java.io.File;
 import java.util.Calendar;
 
 import org.apache.log4j.Logger;
@@ -8,6 +9,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.steve.dataconsolidate.beans.InputDataBean;
+import com.steve.dataconsolidate.common.DCConfig;
+import com.steve.dataconsolidate.common.DCContants;
 import com.steve.dataconsolidate.entity.CourseEntity;
 import com.steve.dataconsolidate.entity.UserCourseEntity;
 import com.steve.dataconsolidate.entity.UserEntity;
@@ -28,66 +31,28 @@ public class DCBootStrap {
 	@Autowired
 	private static DataConsolidateService dataConsolidateService;
 	
+	@Autowired
+	private static DCConfig dcConfig;
+	
 	public static void main(String[] args) {
 		try {
 			String[] path = {"classpath*:META-INF/spring/applicationContext.xml"};
-			ApplicationContext applicaitonContext = new ClassPathXmlApplicationContext(path);
-			genericEntityService = applicaitonContext.getBean(GenericEntityService.class);
-			dataConsolidateService = applicaitonContext.getBean(DataConsolidateService.class);
-			
+			ApplicationContext applicationContext = new ClassPathXmlApplicationContext(path);
+			genericEntityService = applicationContext.getBean(GenericEntityService.class);
+			dataConsolidateService = applicationContext.getBean(DataConsolidateService.class);
+			dcConfig = applicationContext.getBean(DCConfig.class);
 			
 			dataConsolidateService.readProcessAndStoreData(getInputDataBean());
-			
-			
-			/*UserEntity userEntity = (UserEntity)genericEntityService.createEntity(getUserData());
-			int userId = userEntity.getUserId();
-			
-			CourseEntity courseEntity = (CourseEntity)genericEntityService.createEntity(getCourse());
-			int courseId = courseEntity.getCourseId();
-			
-			UserCourseEntity userCourseEntity = (UserCourseEntity) genericEntityService.createEntity(getUserCourse(userId, courseId));
-			System.out.println("======DONE=====");*/
 		} catch (Exception e) {
 			log.info(e.getMessage());
 			e.printStackTrace();
 		}
 	}
 	//-------------HELPERES---------------
-	
 	private static InputDataBean getInputDataBean(){
 		InputDataBean inputDataBean = new InputDataBean();
-		inputDataBean.setDataFilePath("/Users/paspr01/Praveen/projects/Steve/src/main/resources/dataFile.txt");
+		String inputDataDirPath = dcConfig.getInputDataDirPath();
+		inputDataBean.setDataFilePath(inputDataDirPath + File.separator + DCContants.INPUT_DATA_FILE);
 		return inputDataBean;
-	}
-	private static UserEntity getUserData(){
-		UserEntity userEntity = new UserEntity();
-		userEntity.setPersonKey("PersonKey");
-		userEntity.setEmail("emailAddress");
-		userEntity.setFirstName("Firstname");
-		userEntity.setLastName("lastName");
-		Calendar calendar = Calendar.getInstance();
-		userEntity.setCreationDate(calendar.getTime());
-		userEntity.setLastUpdateDate(calendar.getTime());
-		return userEntity;
-	}
-	
-	private static CourseEntity getCourse(){
-		CourseEntity courseEntity = new CourseEntity();
-		courseEntity.setCourseName("courseName");
-		Calendar calendar = Calendar.getInstance();
-		courseEntity.setCreationDate(calendar.getTime());
-		courseEntity.setLastUpdateDate(calendar.getTime());
-		return courseEntity;
-	}
-	
-	private static UserCourseEntity getUserCourse(int userId, int courseId){
-		UserCourseEntity userCourseEntity = new UserCourseEntity();
-		userCourseEntity.setCourseId(courseId);
-		userCourseEntity.setUserId(userId);
-		Calendar calendar = Calendar.getInstance();
-		userCourseEntity.setCreationDate(calendar.getTime());
-		userCourseEntity.setLastUpdateDate(calendar.getTime());
-		userCourseEntity.setCompletionDate(calendar.getTime());
-		return userCourseEntity;
 	}
 }
--- src/main/java/com/steve/dataconsolidate/common/DCContants.java
+++ src/main/java/com/steve/dataconsolidate/common/DCContants.java
@@ -8,4 +8,5 @@ public class DCContants {
 	
 	public static String PERSITENCE_UNIT_DCE = "DCE";
 	
+	public static String INPUT_DATA_FILE = "dataFile.txt";
 }
